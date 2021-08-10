6b4dfc04d901ca86d78965b821c977e0adb9b0a9
JasmineGuy <guyjasmine.c@gmail.com>
2021-08-10 08:57:38 -0500

    initial commit

---
+++ public/js/app.js
@@ -0,0 +1,36 @@
+(function(){
+    
+    function getSessions(){
+        return new Promise(function(resolve, reject){
+          var oReq = new XMLHttpRequest();
+          oReq.onload = function (e) {
+            resolve(e.target.response);
+          };
+          oReq.open('GET', 'sessions.json', true);
+          oReq.responseType = 'json';
+          oReq.send();
+        })
+      }
+     
+      
+      function render() {
+        var list = document.querySelector('#sessions');
+        if (!list) return;
+        list.innerHTML = sessionTemplate(data.listItems);
+      };
+      
+      
+      var data = {
+        listItems: []
+      };
+      
+      getSessions()
+        .then((sessions)=>{
+          console.log('promises!')
+          data.listItems = sessions;
+          render();
+        });
+
+}())
+
+
